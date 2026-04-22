f09811e6103dbf0d16921bcb3b50e4a46db108f9
El Aouina Yassine <urmom@yourMom.local>
2026-04-22 15:48:06 +0100

    mobile page and website page and also django

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('ff')
+import  qrcode
+
+def generateQrCode(sessionId , token):
+    if sessionId and token:
+        img = qrcode.make(f"https://qrcode/presence?session_id={sessionId}&token={token}")
+        return img
+    else:
+        return 0
+
+
